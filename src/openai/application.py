0be10ab591e0ae5d7cf740d65a10dbc515a2c979
Andrian-Loria <134902786+Andrian-Loria@users.noreply.github.com>
2025-04-29 13:26:46 +0700

    Initial commit for integrate branch

---
+++ src/openai/application.py
@@ -0,0 +1,171 @@
+import logging
+import json
+from telegram.ext import *
+from telegram import Update, ReplyKeyboardMarkup
+from crew import researcher
+import asyncio
+import pytz
+from pytz import utc
+import os 
+import sys
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+from dotenv import load_dotenv
+from memoryParser import parse_agent_text
+
+MEMORY_FILE = "memory.json"
+
+MAX_MESSAGE_LENGTH = 4096
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.info('Starting Bot...')
+
+executor = ThreadPoolExecutor()
+ 
+async def start_command(update: Update, context: CallbackContext):
+    user_id = str(update.message.from_user.id)
+    memory = load_memory()
+    
+    if "started_users" not in memory:
+        memory["started_users"] = []
+    
+    if user_id not in memory["started_users"]:
+        memory["started_users"].append(user_id)
+        save_memory(memory)
+    
+    keyboard = [
+        ["/start"],
+        ["/history", "/newchat"]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text(
+        "Halo, selamat datang di aplikasi ini.\nSilahkan pilih menu yang tersedia:",
+        reply_markup=reply_markup
+    )
+    
+def run_crew_blocking(user_input):
+    inputs = {
+        "gejala_user": user_input,
+    }
+    try:
+        result = researcher().crew().kickoff(inputs=inputs)
+        return f"{result}"
+    except Exception as e:
+        return f"Error saat menjalankan CrewAI: {e}"
+
+async def handle_message(update: Update, context: CallbackContext):
+    user_id = str(update.message.from_user.id)
+    user_text = update.message.text
+    memory = load_memory()
+    
+    if "started_users" not in memory or user_id not in memory["started_users"]:
+        await update.message.reply_text("Ketik command terlebih dahulu sebelum memulai chat.")
+        return
+    
+    if user_text == "start":
+        await update.message.reply_text("Silakan ketik gejala yang anda alami.")
+        return
+    elif user_text == "history":
+        await history(update, context)
+        return
+    elif user_text == "newchat":
+        await new_chat(update,context)
+        return
+    
+    await update.message.reply_text("Sedang memproses dengan CrewAI, tunggu sebentar...") 
+
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(executor, run_crew_blocking, user_text)
+
+    await update.message.reply_text(result)
+    memory = load_memory()
+    
+    if user_id not in memory:
+        memory[user_id] = []
+        
+    memory[user_id].append({
+        "user": user_text,
+        "agent": parse_agent_text(result)
+    })
+    save_memory(memory)
+
+async def error(update: Update, context: CallbackContext):
+    print(f"Error terjadi: {context.error}")
+
+async def history(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = str(update.message.from_user.id)
+    memory = load_memory()
+    
+    if user_id in memory and memory[user_id]:
+        history_text = memory[user_id][-10:]
+        formatted_history = ""
+        for item in history_text:
+            if isinstance(item, dict) and "user" in item and "agent" in item:
+                formatted_history += f"User: {item['user']}\nAgent: {item['agent']}\n\n"
+            else:
+                formatted_history += f"[Format tidak valid]: {str(item)}\n\n"
+        try:
+            if len(formatted_history) > MAX_MESSAGE_LENGTH:
+                for i in range(0, len(formatted_history), MAX_MESSAGE_LENGTH):
+                    await update.message.reply_text(formatted_history[i:i+MAX_MESSAGE_LENGTH])
+            else:
+                await update.message.reply_text(f"Histori: \n{formatted_history.strip()}")
+        # await update.message.reply_text(f"Histori: \n{history_text}", parse_mode="Markdown")
+        # await update.message.reply_text(f"Histori: \n{formatted_history.strip()}")
+        except Exception as e:
+            await update.message.reply_text("Pesan terlalu panjang untuk dikirim.")
+    else:
+        await update.message.reply_text("Belum ada histori percakapan.")
+            
+def load_memory():
+    if os.path.exists(MEMORY_FILE):
+        with open(MEMORY_FILE, "r") as f:
+            content = f.read().strip()
+            if content:
+                try:
+                    return json.loads(content)
+                except json.JSONDecodeError as e:
+                    print(f"Gagal membaca memory.json: {e}")
+    return {}
+
+def save_memory(memory):
+    with open(MEMORY_FILE, "w") as f:
+        json.dump(memory,f,indent=4)
+
+async def new_chat(update: Update, context: CallbackContext):
+    user_id = str(update.message.from_user.id)
+    memory = load_memory()
+    
+    if "history_backup" not in memory:
+        memory["history_backup"] = {}
+        
+    if user_id in memory and isinstance(memory[user_id], list):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_key = f"{user_id}_{timestamp}"
+        memory["history_backup"][backup_key] = memory[user_id]
+
+    memory[user_id] = []
+    
+    save_memory(memory)
+
+    await update.message.reply_text("Obrolan baru telah dimulai.")
+    
+def main():
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    application = Application.builder().token(API_KEY).build()
+
+    application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(CommandHandler("newchat", new_chat))
+    # application.add_handler(CommandHandler("custom", custom_command))
+    application.add_handler(CommandHandler("history", history))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_error_handler(error)
+
+    print("Bot is running...")
+    logging.info("Running polling...")
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
+    
---
+++ src/openai/crew.py
@@ -0,0 +1,102 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+# from src.openai.tools.Rag import Search_tool, alo_dokter, halo_doc, halo_sehat, bpom_id, interaksi_obat, penyakit_alo_dokter, penyakit_halo_sehat, icd, who
+from tools.Rag import data_penyakit, data_obat
+
+# If you want to run a snippet of code before or after the crew starts, 
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+
+@CrewBase
+class researcher():
+	"""Openai crew"""
+
+	# Learn more about YAML configuration files here:
+	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+	
+	@agent
+	def analisa_penyakit(self) -> Agent:
+		return Agent(
+			config=self.agents_config['analisa_penyakit'],
+			verbose=True,
+			tools=[data_penyakit],
+			# tools=[penyakit_halo_sehat, penyakit_alo_dokter],
+   			allow_delegation=True,
+			max_iter=30
+		)
+
+	@agent
+	def classification_agent(self) -> Agent:
+		return Agent(
+			config=self.agents_config['classification_agent'],
+			verbose=True,
+			allow_delegation=False,
+			# max_iter=30
+		)
+
+	@agent
+	def apoteker_virtual(self) -> Agent:
+		return Agent(
+			config=self.agents_config['apoteker_virtual'],
+			verbose=True,
+			tools=[data_obat],
+			allow_delegation=False,
+			max_iter=30
+		)
+	
+	@agent
+	def output_manager(self) -> Agent:
+		return Agent(
+			config=self.agents_config['output_manager'],
+			verbose=True,
+			allow_delegation=False,
+			max_iter=30
+		)
+  
+	@task
+	def analisis_penyakit_dari_gejala_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['analisis_penyakit_dari_gejala_task'],
+		)
+
+	@task
+	def classification_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['classification_task'],
+		)
+
+	@task
+	def rekomendasi_obat_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['rekomendasi_obat_task'],
+		)
+	
+	@task
+	def output_manager_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['output_manager_task'],
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Kru AI Virtual Dokter"""
+		return Crew(
+			agents=[
+				self.classification_agent(),
+				self.analisa_penyakit(),
+				self.apoteker_virtual(),
+				self.output_manager()
+			],
+			tasks=[
+				self.classification_task(),
+				self.analisis_penyakit_dari_gejala_task(),
+				self.rekomendasi_obat_task(),
+				self.output_manager_task()
+			],
+			process=Process.sequential,
+        	verbose=True,
+    	)
